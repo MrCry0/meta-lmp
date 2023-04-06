MrCry0/meta-lmp

commit 27af11806edb75af0d590f586af434796bd3d16e

base: sudo: add tmpfiles and drop var with systemd
/var is not preserved when generating ostree-based images, so drop
it and replace the needed folders via tmpfiles (created at runtime).

Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

meta-lmp-base/recipes-extended/sudo/sudo_%.bbappend
@@ -1,0 +1,10 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+SRC_URI += "file://tmpfiles.conf"
+
+do_install:append() {
+	if ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'true', 'false', d)}; then
+		install -D -m 0644 ${WORKDIR}/tmpfiles.conf ${D}${nonarch_libdir}/tmpfiles.d/sudo-vardir.conf
+		(cd ${D}${localstatedir}; rmdir -v --parents ./lib/sudo/lectured)
+	fi
+}

meta-lmp-base/recipes-extended/sudo/sudo/tmpfiles.conf
@@ -1,0 +1,2 @@
+d /var/lib/sudo 0755 root root -
+d /var/lib/sudo/lectured 0700 root root -

commit 72c63adef2391c56571050a5fa2d756350a33ccc

base: sudo: do not try to remove current folder
rmdir cannot remove "." folder with --parents option [1]. Omit
removing ".".

[1]
rmdir: removing directory, '.'
rmdir: failed to remove directory '.': Invalid argument

Fixes: 27af1180 ("base: sudo: add tmpfiles and drop var with systemd")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-base/recipes-extended/sudo/sudo_%.bbappend
@@ -5,6 +5,6 @@ SRC_URI += "file://tmpfiles.conf"
 do_install:append() {
 	if ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'true', 'false', d)}; then
 		install -D -m 0644 ${WORKDIR}/tmpfiles.conf ${D}${nonarch_libdir}/tmpfiles.d/sudo-vardir.conf
-		(cd ${D}${localstatedir}; rmdir -v --parents ./lib/sudo/lectured)
+		(cd ${D}${localstatedir}; rmdir -v --parents lib/sudo/lectured)
 	fi
 }