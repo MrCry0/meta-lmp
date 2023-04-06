MrCry0/meta-lmp

commit 70eaaef13f455308c229f74ae5ff202a2b31915a

base: softhsm: drop /var folders with systemd
Files are instead created at runtime via systemd tmpfiles.d.

Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

meta-lmp-base/recipes-security/softhsm/softhsm_2.%.bbappend
@@ -7,6 +7,7 @@ SRC_URI:append = " \
 do_install:append() {
 	if ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'true', 'false', d)}; then
 		install -D -m 0644 ${WORKDIR}/tmpfiles.conf ${D}${nonarch_libdir}/tmpfiles.d/softhsm.conf
+		(cd ${D}${localstatedir}; rmdir -v --parents ./lib/softhsm/tokens)
 	fi
 }
 

commit 262999dea9d9440cf2ae38c3df799265eb01b8f1

base: softhsm: do not try to remove current folder
rmdir cannot remove "." folder with --parents option [1]. Omit
removing ".".

[1]
rmdir: removing directory, '.'
rmdir: failed to remove directory '.': Invalid argument

Fixes: 70eaaef1 ("base: softhsm: drop /var folders with systemd")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-base/recipes-security/softhsm/softhsm_2.%.bbappend
@@ -7,7 +7,7 @@ SRC_URI:append = " \
 do_install:append() {
 	if ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'true', 'false', d)}; then
 		install -D -m 0644 ${WORKDIR}/tmpfiles.conf ${D}${nonarch_libdir}/tmpfiles.d/softhsm.conf
-		(cd ${D}${localstatedir}; rmdir -v --parents ./lib/softhsm/tokens)
+		(cd ${D}${localstatedir}; rmdir -v --parents lib/softhsm/tokens)
 	fi
 }
 