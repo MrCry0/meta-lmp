MrCry0/meta-lmp

commit 27af11806edb75af0d590f586af434796bd3d16e

base: sudo: add tmpfiles and drop var with systemd
/var is not preserved when generating ostree-based images, so drop
it and replace the needed folders via tmpfiles (created at runtime).

Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

meta-lmp-base/recipes-extended/sudo/sudo_%.bbappend
@@ -1,0 +1,10 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+SRC_URI += "file://tmpfiles.conf"
+
+do_install:append() {
+	if ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'true', 'false', d)}; then
+		install -D -m 0644 ${WORKDIR}/tmpfiles.conf ${D}${nonarch_libdir}/tmpfiles.d/sudo-vardir.conf
+		(cd ${D}${localstatedir}; rmdir -v --parents ./lib/sudo/lectured)
+	fi
+}

meta-lmp-base/recipes-extended/sudo/sudo/tmpfiles.conf
@@ -1,0 +1,2 @@
+d /var/lib/sudo 0755 root root -
+d /var/lib/sudo/lectured 0700 root root -

commit 0aa92b2de511047467dce480c2ff7fa3ea4a5f62

base: sudo: include tempfiles config
Include sudo-vardir.conf in package to create necessary folders
by systemd tempfiles service.

Fixes: 27af1180 ("base: sudo: add tmpfiles and drop var with systemd")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-base/recipes-extended/sudo/sudo_%.bbappend
@@ -8,3 +8,5 @@ do_install:append() {
 		(cd ${D}${localstatedir}; rmdir -v --parents lib/sudo/lectured)
 	fi
 }
+
+FILES:${PN} += "${nonarch_libdir}/tmpfiles.d/sudo-vardir.conf"