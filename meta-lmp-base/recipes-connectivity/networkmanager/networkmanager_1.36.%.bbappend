MrCry0/meta-lmp

commit f6528b5ed635c0191d21044d7e4d2c40d4d38a2f

base: networkmanager: drop /var/lib/NetworkManager folder
/var is not preserved when generating ostree-based images and NM is able
to dynamically create the required folder at runtime.

Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

meta-lmp-base/recipes-connectivity/networkmanager/networkmanager_1.36.%.bbappend
@@ -6,3 +6,8 @@ PACKAGECONFIG[firewalld-zone] = "-Dfirewalld_zone=true,-Dfirewalld_zone=false"
 SRC_URI:append = " \
 	file://0001-85-nm-unmanaged.rules-do-not-manage-docker-bridges.patch \
 "
+
+do_install:append() {
+	# NM is able to create /var/lib/NetworkManager on runtime
+	(cd ${D}${localstatedir}; rmdir -v --parents ./lib/NetworkManager)
+}

commit 893f5f8354ee7b7d694c19d72564d2573005f33b

base: networkmanager: do not try to remove current folder
rmdir cannot remove "." folder with --parents option [1]. Omit
removing ".".

[1]
rmdir: removing directory, '.'
rmdir: failed to remove directory '.': Invalid argument

Fixes: f6528b5e ("base: networkmanager: drop /var/lib/NetworkManager folder")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-base/recipes-connectivity/networkmanager/networkmanager_1.36.%.bbappend
@@ -9,5 +9,5 @@ SRC_URI:append = " \
 
 do_install:append() {
 	# NM is able to create /var/lib/NetworkManager on runtime
-	(cd ${D}${localstatedir}; rmdir -v --parents ./lib/NetworkManager)
+	(cd ${D}${localstatedir}; rmdir -v --parents lib/NetworkManager)
 }